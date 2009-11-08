17dc4e01acdd5a8253f4d0a41e46484e243604cc
dh <me@d-harrington.com>
2009-11-08 17:47:31 +0100

    move options into their own class

---
+++ lib/savon/options.rb
@@ -0,0 +1,102 @@
+module Savon
+  class Options
+
+    # Supported SOAP versions.
+    SoapVersions = [1, 2]
+
+    # The default SOAP version to use.
+    DefaultSoapVersion = 1
+
+    # The default XML root node to use.
+    DefaultRootNode = "//return"
+
+    # Returns the endpoint.
+    attr_reader :endpoint
+
+    # Sets the endpoint to the given +endpoint+.
+    def endpoint=(endpoint)
+      raise ArgumentError, "Invalid endpoint: #{endpoint}" unless valid_endpoint? endpoint
+      @endpoint = URI endpoint
+    end
+
+    # Returns the SOAP version. Defaults to +DefaultSoapVersion+.
+    def soap_version
+      @soap_version || DefaultSoapVersion
+    end
+
+    # Sets the SOAP version to the given +soap_version+.
+    def soap_version=(soap_version)
+      raise ArgumentError, "Invalid SOAP version: #{soap_version}" unless valid_soap_version? soap_version
+      @soap_version = soap_version
+    end
+
+    # Returns the XML root node. Defaults to +DefaultRootNode+.
+    def root_node
+      @root_node || DefaultRootNode
+    end
+
+    # Sets the root node to the given +root_node+.
+    def root_node=(root_node)
+      @root_node = root_node if root_node.kind_of? String
+    end
+
+    # Sets whether the SOAP response should be returned as pure XML.
+    attr_writer :xml_response
+
+    # Returns whether the SOAP response should be returned as pure XML.
+    def xml_response?
+      @xml_response
+    end
+
+    # Returns the WSSE username.
+    attr_reader :wsse_username
+
+    def wsse_username=(wsse_username)
+      @wsse_username = wsse_username if wsse_username.kind_of? String
+    end
+
+    # Returns the WSSE password.
+    attr_reader :wsse_password
+
+    def wsse_password=(wsse_password)
+      @wsse_password = wsse_password if wsse_password.kind_of? String
+    end
+
+    # Sets whether WSSE digest should be used. If enabled, the WSSE password
+    # will be encrypted automatically.
+    attr_writer :wsse_digest
+
+    # Returns whether WSSE digest should be used.
+    def wsse_digest?
+      @wsse_digest
+    end
+
+    # Returns whether WSSE authentication should be used.
+    def wsse?
+      wsse_username && wsse_password
+    end
+
+    # Sets options from a given Hash of +options+.
+    def from_hash(options)
+      all_options.each { |option| self.send option, options[options] }
+    end
+
+  private
+
+    # Returns an Array of all available options.
+    def all_options
+      [:soap_version, :root_node, :xml_response, :wsse_username, :wsse_password, :wsse_digest]
+    end
+
+    # Returns whether a given +endpoint+ is valid.
+    def valid_endpoint?(endpoint)
+      /^http.+/ === endpoint
+    end
+
+    # Returns whether a given +soap_version+ is valid.
+    def valid_soap_version?(soap_version)
+      soap_version.kind_of?(Fixnum) && SoapVersions.include?(soap_version)
+    end
+
+  end
+end
