7d78bd7f56c73376124657b388e6a9ea37716957
dh <me@d-harrington.com>
2009-11-09 21:18:24 +0100

    response

--- lib/savon/wsdl.rb
+++ lib/savon/wsdl.rb
@@ -1,7 +1,15 @@
+require 'rubygems'
+require 'hpricot'
+
 module Savon
 
   # Savon::WSDL represents the WSDL document.
   class WSDL
+
+    # Initializer expects instances of Savon::Options and Savon::HTTP.
+    def initialize(options, http)
+      @options, @http = options, http
+    end
 
     # Returns the namespace URI.
     def namespace_uri
@@ -18,39 +26,43 @@ module Savon
       @choice_elements ||= parse_choice_elements
     end
 
-    # Initializer expects the endpoint +uri+ and a Net::HTTP instance (+http+).
-    def initialize(uri, http)
-      @uri, @http = uri, http
-    end
-
     # Returns the body of the Net::HTTPResponse from the WSDL request.
     def to_s
-      @response ? @response.body : nil
+      wsdl_response ? wsdl_response.body : nil
     end
 
   private
 
+    def wsdl_response
+      unless @wsdl_response
+        @wsdl_response = @http.retrieve_wsdl
+        validate_wsdl!
+      end
+      @wsdl_response
+    end
+
     # Returns an Hpricot::Document of the WSDL. Retrieves the WSDL from the
     # endpoint URI in case it wasn't retrieved already.
-    def document
-      unless @document
-        @response = @http.get("#{@uri.path}?#{@uri.query}")
-        @document = Hpricot.XML(@response.body)
-        raise ArgumentError, "Unable to find WSDL at: #{@uri}" if
-          !soap_actions || soap_actions.empty?
+    def wsdl_document
+      @wsdl_document = Hpricot.XML(wsdl_response.body) unless @wsdl_document
+      @wsdl_document
+    end
+
+    def validate_wsdl!
+      if !soap_actions || soap_actions.empty?
+        raise ArgumentError, "Unable to find WSDL at: #{@options.endpoint}"
       end
-      @document
     end
 
     # Parses the WSDL for the namespace URI.
     def parse_namespace_uri
-      definitions = document.at('//wsdl:definitions')
+      definitions = wsdl_document.at('//wsdl:definitions')
       definitions.get_attribute('targetNamespace') if definitions
     end
 
     # Parses the WSDL for available SOAP actions.
     def parse_soap_actions
-      soap_actions = document.search('[@soapAction]')
+      soap_actions = wsdl_document.search('[@soapAction]')
 
       soap_actions.collect do |soap_action|
         soap_action.parent.get_attribute('name')
@@ -59,7 +71,7 @@ module Savon
 
     # Parses the WSDL for choice elements.
     def parse_choice_elements
-      choice_elements = document.search('//xs:choice//xs:element')
+      choice_elements = wsdl_document.search('//xs:choice//xs:element')
 
       choice_elements.collect do |choice_element|
         choice_element.get_attribute('ref').sub(/(.+):/, '')
