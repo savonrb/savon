e36c35fd0b4bb751d0bf9a90d25e3f93b70b4834
dh <me@d-harrington.com>
2009-11-28 16:02:03 +0100

    specs and rdoc

--- lib/savon/soap.rb
+++ lib/savon/soap.rb
@@ -11,44 +11,58 @@ module Savon
     # Content-Types by SOAP version.
     ContentType = { 1 => "text/xml", 2 => "application/soap+xml" }
 
+    # The default SOAP version to use.
+    @version = 1
+
+    class << self
+      # Accessor for the default SOAP version to use.
+      attr_accessor :version
+    end
+
+    # Expects a SOAP +action+, +body+, +options+ and the +namespace_uri+.
     def initialize(action, body, options, namespace_uri)
       @action, @body = action, body
       @options, @namespace_uri = options, namespace_uri
     end
 
-    attr_reader :action, :options
+    # Returns the SOAP action.
+    attr_reader :action
 
+    # Returns the SOAP options.
+    attr_reader :options
+
+    # Returns the XML for a SOAP request.
     def body
+      builder = Builder::XmlMarkup.new
+
       @xml_body ||= builder.env :Envelope, envelope_namespaces do |xml|
         xml.env(:Header) { envelope_header xml }
         xml.env(:Body) { envelope_body xml }
       end
     end
 
+    # Returns the SOAP version to use.
     def version
-      options[:soap_version] || 1 # TODO: default to a class instance variable
+      @options[:soap_version] || self.class.version
     end
 
   private
 
-    def builder
-      @builder ||= Builder::XmlMarkup.new
-    end
-
+    # Returns a Hash of namespaces for the SOAP envelope.
     def envelope_namespaces
       { "xmlns:env" => SOAPNamespace[version], "xmlns:wsdl" => @namespace_uri }
     end
 
+    # Expects an instance of Builder::XmlMarkup and returns the XML for the
+    # SOAP envelope header.
     def envelope_header(xml)
       wsse_header xml if wsse?
     end
 
+    # Expects an instance of Builder::XmlMarkup and returns the XML for the
+    # SOAP envelope body.
     def envelope_body(xml)
-      xml.wsdl(@action.to_sym) { xml << translate_body }
-    end
-
-    def translate_body
-      @body.to_soap_xml rescue @body.to_s
+      xml.wsdl(@action.to_sym) { xml << @body.to_soap_xml rescue @body.to_s }
     end
 
   end
---
+++ spec/savon/soap_spec.rb
@@ -0,0 +1,86 @@
+require "spec_helper"
+
+describe Savon::SOAP do
+  before { @soap = new_soap_instance }
+
+  def new_soap_instance(options = {})
+    Savon::SOAP.new UserFixture.soap_actions[:find_user], { :id => 666 },
+      options, UserFixture.namespace_uri
+  end
+
+  describe "SOAPNamespace" do
+    it "contains the SOAP namespace for each supported SOAP version" do
+      Savon::Validation::SOAPVersions.each do |soap_version|
+        Savon::SOAP::SOAPNamespace[soap_version].should_not be_nil
+      end
+    end
+  end
+
+  describe "ContentType" do
+    it "contains the Content-Types for each supported SOAP version" do
+      Savon::Validation::SOAPVersions.each do |soap_version|
+        Savon::SOAP::ContentType[soap_version].should_not be_nil
+      end
+    end
+  end
+
+  describe "initialize" do
+    it "expects the SOAP action, body, options and the namespace URI" do
+      new_soap_instance
+    end
+  end
+
+  describe "action" do
+    it "returns the SOAP action" do
+      @soap.action.should == UserFixture.soap_actions[:find_user]
+    end
+  end
+
+  describe "options" do
+    it "returns the SOAP options" do
+      @soap.options.should == {}
+    end
+  end
+
+  describe "body" do
+    before { Savon::SOAP.version = 1 }
+
+    it "returns the XML for a SOAP request" do
+      @soap.body.should == soap_body
+    end
+
+    it "caches the XML, returning the same Object every time" do
+      @soap.body.object_id.should == @soap.body.object_id
+    end
+
+    it "uses the SOAP namespace for the SOAP version passed in via options" do
+      soap = new_soap_instance :soap_version => 2
+      soap.body.should include Savon::SOAP::SOAPNamespace[2]
+    end
+
+    it "uses the SOAP namespace for the default SOAP version otherwise" do
+      Savon::SOAP.version = 2
+      @soap.body.should include Savon::SOAP::SOAPNamespace[2]
+    end
+
+    def soap_body
+      "<env:Envelope xmlns:wsdl=\"http://v1_0.ws.user.example.com\" " <<
+        "xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\">" <<
+        "<env:Header></env:Header>" <<
+        "<env:Body><wsdl:findUser><id>666</id></wsdl:findUser></env:Body>" <<
+      "</env:Envelope>"
+    end
+  end
+
+  describe "version" do
+    it "returns the SOAP version from options" do
+      soap = new_soap_instance :soap_version => 2
+      soap.version.should == 2
+    end
+
+    it "returns the default SOAP version otherwise" do
+      @soap.version.should == Savon::SOAP.version
+    end
+  end
+
+end
