dd13891cb9bacba39fba3ab72cf9fc543e9333db
rubiii <me@rubiii.com>
2009-12-19 19:08:57 +0100

    changed from class instance to class methods

--- spec/endpoint_helper.rb
+++ spec/endpoint_helper.rb
@@ -1,18 +1,16 @@
 class EndpointHelper
-  class << self
 
-    def wsdl_endpoint(type = nil)
-      soap_endpoint(type) << "?wsdl"
-    end
-
-    def soap_endpoint(type = nil)
-      case type
-        when :soap_fault then "http://soapfault.example.com/Service"
-        when :http_error then "http://httperror.example.com/Service"
-        when :invalid    then "http://invalid.example.com/Service"
-        else                  "http://validation.example.com/Service"
-      end
-    end
-
+  def self.wsdl_endpoint(type = nil)
+    soap_endpoint(type) << "?wsdl"
   end
+
+  def self.soap_endpoint(type = nil)
+    case type
+      when :soap_fault then "http://soapfault.example.com/Service"
+      when :http_error then "http://httperror.example.com/Service"
+      when :invalid    then "http://invalid.example.com/Service"
+      else                  "http://validation.example.com/Service"
+    end
+  end
+
 end
