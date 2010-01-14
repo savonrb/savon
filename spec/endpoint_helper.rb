d643fd725ca365e69e6302d4048c051fdd47e4b7
rubiii <me@rubiii.com>
2010-01-14 23:50:11 +0100

    fixed endpoints to reflect latest changes to fix github issue #21

--- spec/endpoint_helper.rb
+++ spec/endpoint_helper.rb
@@ -5,16 +5,16 @@ class EndpointHelper
     case type
       when :no_namespace       then "http://nons.example.com/Service?wsdl"
       when :namespaced_actions then "http://nsactions.example.com/Service?wsdl"
-      else                          soap_endpoint(type) + "?wsdl"
+      else                          soap_endpoint(type)
     end
   end
 
   # Returns the SOAP endpoint for a given +type+ of request.
   def self.soap_endpoint(type = nil)
     case type
-      when :soap_fault then "http://soapfault.example.com/Service"
-      when :http_error then "http://httperror.example.com/Service"
-      when :invalid    then "http://invalid.example.com/Service"
+      when :soap_fault then "http://soapfault.example.com/Service?wsdl"
+      when :http_error then "http://httperror.example.com/Service?wsdl"
+      when :invalid    then "http://invalid.example.com/Service?wsdl"
       else                  "http://example.com/validation/1.0/AuthenticationService"
     end
   end
