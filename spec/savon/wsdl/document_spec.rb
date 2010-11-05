f54de4337e378086fa45063a44fb5693bde8cce3
rubiii <me@rubiii.com>
2010-11-05 13:15:38 +0100

    improved wsdl document and parser specs

--- spec/savon/wsdl/document_spec.rb
+++ spec/savon/wsdl/document_spec.rb
@@ -2,25 +2,73 @@ require "spec_helper"
 
 describe Savon::WSDL::Document do
 
-  context "with a remote document" do
-    let(:wsdl) { Savon::WSDL::Document.new HTTPI::Request.new, Endpoint.wsdl }
-    before { HTTPI.stubs(:get).returns(new_response) }
-
+  shared_examples_for "a WSDL document" do
     it "should be present" do
       wsdl.should be_present
     end
 
     describe "#namespace" do
       it "should return the namespace URI" do
-        wsdl.namespace.should == WSDLFixture.authentication(:namespace)
+        wsdl.namespace.should == "http://v1_0.ws.auth.order.example.com/"
       end
     end
 
     describe "#soap_actions" do
       it "should return an Array of available SOAP actions" do
-        wsdl.soap_actions.should include(*WSDLFixture.authentication(:operations).keys)
+        wsdl.soap_actions.should == [:authenticate]
       end
     end
+
+    describe "#soap_action" do
+      it "should return the SOAP action for a given key" do
+        wsdl.soap_action(:authenticate).should == "authenticate"
+      end
+
+      it "should return nil if no SOAP action could be found" do
+        wsdl.soap_action(:unknown).should be_nil
+      end
+    end
+
+    describe "#soap_input" do
+      it "should return the SOAP input tag for a given key" do
+        wsdl.soap_input(:authenticate).should == :authenticate
+      end
+
+      it "should return nil if no SOAP input tag could be found" do
+        wsdl.soap_input(:unknown).should be_nil
+      end
+    end
+
+    describe "#operations" do
+      it "should return a Hash of SOAP operations" do
+        wsdl.operations.should == {
+          :authenticate => {
+            :input => "authenticate", :action => "authenticate"
+          }
+        }
+      end
+    end
+
+    describe "#document" do
+      it "should return the raw WSDL document" do
+        wsdl.document.should == WSDLFixture.load
+      end
+
+      it "should be memoized" do
+        wsdl.document.should equal(wsdl.document)
+      end
+    end
+  end
+
+  context "with a remote document" do
+    let(:wsdl) { Savon::WSDL::Document.new HTTPI::Request.new, Endpoint.wsdl }
+
+    before do
+      response = HTTPI::Response.new(200, {}, WSDLFixture.load)
+      HTTPI.stubs(:get).returns(response)
+    end
+
+    it_should_behave_like "a WSDL document"
   end
 
   context "with a local document" do
@@ -31,114 +79,33 @@ describe Savon::WSDL::Document do
 
     before { HTTPI.expects(:get).never }
 
-    it "should be present" do
-      wsdl.should be_present
+    it_should_behave_like "a WSDL document"
+  end
+
+  context "without a WSDL document" do
+    let(:wsdl) { Savon::WSDL::Document.new HTTPI::Request.new }
+
+    it "should not be present" do
+      wsdl.should_not be_present
     end
 
-    describe "#namespace" do
-      it "should return the namespace URI" do
-        wsdl.namespace.should == WSDLFixture.authentication(:namespace)
+    describe "#soap_action" do
+      it "should return nil" do
+        wsdl.soap_action(:authenticate).should be_nil
       end
     end
 
-    describe "#soap_actions" do
-      it "should return an Array of available SOAP actions" do
-        wsdl.soap_actions.should include(*WSDLFixture.authentication(:operations).keys)
+    describe "#soap_input" do
+      it "should return nil" do
+        wsdl.soap_input(:authenticate).should be_nil
+      end
+    end
+
+    describe "#document" do
+      it "should raise an ArgumentError" do
+        lambda { wsdl.document }.should raise_error(ArgumentError)
       end
     end
   end
-
-  def new_response(options = {})
-    defaults = { :code => 200, :headers => {}, :body => WSDLFixture.load }
-    response = defaults.merge options
-    
-    HTTPI::Response.new(response[:code], response[:headers], response[:body])
-  end
-
-end
-__END__
-
-    it "has a getter for the namespace URI" do
-      @wsdl.namespace.should == WSDLFixture.authentication(:namespace_uri)
-    end
-
-    it "has a getter for returning an Array of available SOAP actions" do
-      WSDLFixture.authentication(:operations).keys.each do |soap_action|
-        @wsdl.soap_actions.should include(soap_action)
-      end
-    end
-
-    it "has a getter for returning a Hash of available SOAP operations" do
-      @wsdl.operations.should == WSDLFixture.authentication(:operations)
-    end
-
-    it "responds to SOAP actions while still behaving as usual otherwise" do
-      WSDLFixture.authentication(:operations).keys.each do |soap_action|
-        @wsdl.respond_to?(soap_action).should be_true
-      end
-
-      @wsdl.respond_to?(:object_id).should be_true
-      @wsdl.respond_to?(:some_undefined_method).should be_false
-    end
-
-    it "returns the raw WSDL document for to_xml" do
-      @wsdl.to_xml.should == WSDLFixture.authentication
-    end
-  end
-
-  describe "a WSDL document having core sections without a namespace" do
-    before { @wsdl = new_wsdl :no_namespace }
-
-    it "returns the namespace URI" do
-      @wsdl.namespace_uri.should == WSDLFixture.no_namespace(:namespace_uri)
-    end
-
-    it "returns an Array of available SOAP actions" do
-      WSDLFixture.no_namespace(:operations).keys.each do |soap_action|
-        @wsdl.soap_actions.should include(soap_action)
-      end
-    end
-
-    it "returns a Hash of SOAP operations" do
-      @wsdl.operations.should == WSDLFixture.no_namespace(:operations)
-    end
-  end
-
-  describe "a WSDL document with namespaced SOAP actions" do
-    before { @wsdl = new_wsdl :namespaced_actions }
-
-    it "returns the namespace URI" do
-      @wsdl.namespace_uri.should == WSDLFixture.namespaced_actions(:namespace_uri)
-    end
-
-    it "returns an Array of available SOAP actions" do
-      WSDLFixture.namespaced_actions(:operations).keys.each do |soap_action|
-        @wsdl.soap_actions.should include(soap_action)
-      end
-    end
-
-    it "returns a Hash of SOAP operations" do
-      @wsdl.operations.should == WSDLFixture.namespaced_actions(:operations)
-    end
-  end
-
-  describe "a WSDL document from geotrust" do
-    before { @wsdl = new_wsdl :geotrust }
-    
-    it "returns the namespace URI" do
-      @wsdl.namespace_uri.should == WSDLFixture.geotrust(:namespace_uri)
-    end
-    
-    it "returns an Array of available SOAP actions" do
-      WSDLFixture.geotrust(:operations).keys.each do |soap_action|
-        @wsdl.soap_actions.should include(soap_action)
-      end
-    end
-    
-    it "returns a Hash of SOAP operations" do
-      @wsdl.operations.should == WSDLFixture.geotrust(:operations)
-    end
-  end
-
 
 end
