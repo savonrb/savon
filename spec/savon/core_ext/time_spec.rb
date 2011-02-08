0fec7acaec67b70f82d319c6c93f535de0a06be0
Jim Kingdon <jkingdon@localhost.localdomain>
2011-02-07 22:23:14 -0500

    Fix time formatting to match XML Schema spec.

--- spec/savon/core_ext/time_spec.rb
+++ spec/savon/core_ext/time_spec.rb
@@ -6,7 +6,7 @@ describe Time do
     let(:time) { Time.utc(2011, 01, 04, 13, 45, 55) }
 
     it "should return an xs:dateTime formatted String" do
-      time.xs_datetime.should == "2011-01-04T13:45:55UTC"
+      time.xs_datetime.should == "2011-01-04T13:45:55Z"
     end
   end
 
