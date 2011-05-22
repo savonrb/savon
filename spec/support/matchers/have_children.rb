c8761f72743bc30ca7a2b1916d0d6140636ead9c
rubiii <me@rubiii.com>
2011-05-22 20:25:26 +0200

    renamed helper method in have_children matcher

--- spec/support/matchers/have_children.rb
+++ spec/support/matchers/have_children.rb
@@ -1,24 +1,24 @@
 RSpec::Matchers.define :have_children do |tags|
   match do |node|
     results = tags.map do |name, value|
-      node.children.any? { |child| child.to_s == xml_nodes(name => value) }
+      node.children.any? { |child| child.to_s == to_tags(name => value) }
     end
     !results.any? { |result| result == false }
   end
 
   failure_message_for_should do |actual|
-    "expected that #{actual} includes #{xml_nodes(expected)}"
+    "expected that #{actual} includes #{to_tags(expected)}"
   end
 
   failure_message_for_should_not do |actual|
-    "expected that #{actual} does not include #{xml_nodes(expected)}"
+    "expected that #{actual} does not include #{to_tags(expected)}"
   end
 
   description do
-    "include #{xml_nodes(expected)}"
+    "include #{to_tags(expected)}"
   end
 
-  def xml_nodes(tags)
+  def to_tags(tags)
     tags = tags.first if tags.kind_of?(Array)
     tags.map { |name, value| "<#{name}>#{value}</#{name}>" }.join(", ")
   end
