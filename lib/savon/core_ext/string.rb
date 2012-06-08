a374312f7884b80953fb7bcba4e3d03a4f757cf3
rubiii <me@rubiii.com>
2012-06-08 15:18:33 +0200

    don't define String#snakecase if already defined

--- lib/savon/core_ext/string.rb
+++ lib/savon/core_ext/string.rb
@@ -4,17 +4,24 @@ module Savon
   module CoreExt
     module String
 
-      # Returns the String in snake_case.
-      def snakecase
-        str = dup
-        str.gsub! /::/, '/'
-        str.gsub! /([A-Z]+)([A-Z][a-z])/, '\1_\2'
-        str.gsub! /([a-z\d])([A-Z])/, '\1_\2'
-        str.tr! ".", "_"
-        str.tr! "-", "_"
-        str.downcase!
-        str
-      end unless method_defined?(:snakecase)
+      def self.included(base)
+        unless "savon".respond_to?(:snakecase)
+          base.send(:include, Extension)
+        end
+      end
+
+      module Extension
+        def snakecase
+          str = dup
+          str.gsub! /::/, '/'
+          str.gsub! /([A-Z]+)([A-Z][a-z])/, '\1_\2'
+          str.gsub! /([a-z\d])([A-Z])/, '\1_\2'
+          str.tr! ".", "_"
+          str.tr! "-", "_"
+          str.downcase!
+          str
+        end
+      end
 
     end
   end
