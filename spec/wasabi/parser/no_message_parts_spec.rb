aceed19a683a77d79e4787588cb18f7ffd31680e
hoverlover <hoverlover@gmail.com>
2012-06-15 09:41:04 -0500

    Fixes issue with WSDL message element having no part element.

---
+++ spec/wasabi/parser/no_message_parts_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe Wasabi::Parser do
+  context "with: no_message_parts.wsdl" do
+
+    subject do
+      parser = Wasabi::Parser.new Nokogiri::XML(xml)
+      parser.parse
+      parser
+    end
+
+    let(:xml) { fixture(:no_message_parts).read }
+
+    it "falls back to using the message type in the port element" do
+      subject.operations[:save][:input].should == "SaveSoapIn"
+    end
+
+    it "falls back to using the namespace ID in the port element" do
+      subject.operations[:save][:namespace_identifier].should == "actions"
+    end
+  end
+end
