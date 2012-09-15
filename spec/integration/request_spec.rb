c9ce1e19877c1ac46c553924c65b63ba8daf5751
rubiii <me@rubiii.com>
2012-09-15 10:37:26 +0200

    refactored integration specs
    added timeouts for every request

--- spec/integration/request_spec.rb
+++ spec/integration/request_spec.rb
@@ -2,46 +2,60 @@ require "spec_helper"
 
 describe "Integration" do
 
-  it "returns the result in a CDATA tag" do
-    client = Savon.client("http://www.webservicex.net/stockquote.asmx?WSDL")
-    response = client.request(:get_quote, :body => { :symbol => "AAPL" })
+  subject(:client) {
+    client = Savon.client(service_endpoint)
+    client.http.open_timeout = 10
+    client.http.read_timeout = 10
+    client
+  }
 
-    cdata = response[:get_quote_response][:get_quote_result]
-    result = Nori.parse(cdata)
-    result[:stock_quotes][:stock][:symbol].should == "AAPL"
-  end
+  context "stockquote" do
+    let(:service_endpoint) { "http://www.webservicex.net/stockquote.asmx?WSDL" }
 
-  it "passes Strings as they are" do
-    client = Savon.client("http://ws.cdyne.com/emailverify/Emailvernotestemail.asmx?wsdl")
-    response = client.request(:verify_email, :body => { :email => "soap@example.com", "LicenseKey" => "?" })
+    it "returns the result in a CDATA tag" do
+      response = client.request(:get_quote, :body => { :symbol => "AAPL" })
 
-    response_text = response[:verify_email_response][:verify_email_result][:response_text]
-    response_text.should == "Email Domain Not Found"
-  end
-
-  it "supports threads making requests simultaneously" do
-    client = Savon::Client.new do
-        wsdl.document = "http://www.thomas-bayer.com/axis2/services/BLZService?wsdl"
+      cdata = response[:get_quote_response][:get_quote_result]
+      result = Nori.parse(cdata)
+      result[:stock_quotes][:stock][:symbol].should == "AAPL"
     end
+  end
 
-    mutex = Mutex.new
+  context "email" do
+    let(:service_endpoint) { "http://ws.cdyne.com/emailverify/Emailvernotestemail.asmx?wsdl" }
 
-    request_data = [70070010, 24050110, 20050550]
-    threads_waiting = request_data.size
+    it "passes Strings as they are" do
+      response = client.request(:verify_email, :body => { :email => "soap@example.com", "LicenseKey" => "?" })
 
-    threads = request_data.map do |blz|
-      Thread.new do
-        response = client.request :get_bank, :body => { :blz => blz }
-        Thread.current[:value] = response.to_hash[:get_bank_response][:details]
-        mutex.synchronize { threads_waiting -= 1 }
+      response_text = response[:verify_email_response][:verify_email_result][:response_text]
+      response_text.should == "Email Domain Not Found"
+    end
+  end
+
+  context "zip code" do
+    let(:service_endpoint) { "http://www.thomas-bayer.com/axis2/services/BLZService?wsdl" }
+
+    it "supports threads making requests simultaneously" do
+      mutex = Mutex.new
+
+      request_data = [70070010, 24050110, 20050550]
+      threads_waiting = request_data.size
+
+      threads = request_data.map do |blz|
+        Thread.new do
+          response = client.request :get_bank, :body => { :blz => blz }
+          Thread.current[:value] = response[:get_bank_response][:details]
+          mutex.synchronize { threads_waiting -= 1 }
+        end
       end
+
+      sleep(1) until threads_waiting == 0
+
+      threads.each &:kill
+      values = threads.map { |thr| thr[:value] }.compact
+
+      values.uniq.size.should == values.size
     end
-
-    sleep(1) until threads_waiting == 0
-
-    threads.each &:kill
-    values = threads.map { |thr| thr[:value] }.compact
-
-    values.uniq.size.should == values.size
   end
+
 end
