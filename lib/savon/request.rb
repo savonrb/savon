77efa7cd4d7c075c9ece89a2a186567000cf4635
rubiii <me@rubiii.com>
2012-12-13 18:48:12 +0100

    inlined the Request's #content_type method

--- lib/savon/request.rb
+++ lib/savon/request.rb
@@ -42,16 +42,12 @@ module Savon
 
       http.headers = @globals[:headers] if @globals.include? :headers
       http.headers["SOAPAction"] ||= %{"#{@locals[:soap_action]}"} if @locals.include? :soap_action
-      http.headers["Content-Type"] = content_type
+      http.headers["Content-Type"] = CONTENT_TYPE[@globals[:soap_version]] % @globals[:encoding]
 
       http.auth.basic *@globals[:basic_auth] if @globals.include? :basic_auth
       http.auth.digest *@globals[:digest_auth] if @globals.include? :digest_auth
 
       http
-    end
-
-    def content_type
-      CONTENT_TYPE[@globals[:soap_version]] % @globals[:encoding]
     end
 
     def log_request(url, headers, body)
