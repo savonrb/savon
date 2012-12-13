d5b7229dfcf744b37d18eecce31fd8bf40cc8eed
rubiii <me@rubiii.com>
2012-12-13 16:16:29 +0100

    rm the version 1 interface from the Savon module
    i really don't feel like it's worth to build the new interface alongside
    the current one. the original plan was to add the new interface to upcoming
    version 1.x releases and to remove it from version 2.
    
    this would require me to maintain one interface in two versions plus the
    version 1 interface for quite some time. given that i don't have too much
    time maintaining this project in my free time, a clear cut and update path
    sounds more realistic to me.

--- lib/savon.rb
+++ lib/savon.rb
@@ -1,28 +1,11 @@
 require "savon/version"
-require "savon/config"
-require "savon/client"
 require "savon/new_client"
 require "savon/model"
 
 module Savon
-  extend self
 
-  def client(*args, &block)
-    Client.new(*args, &block)
-  end
-
-  def new_client(globals = {})
+  def self.new_client(globals = {})
     NewClient.new(globals)
   end
-
-  def configure
-    yield config
-  end
-
-  def config
-    @config ||= Config.default
-  end
-
-  attr_writer :config
 
 end
