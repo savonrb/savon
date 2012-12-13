d23b6c1ec11bdbba7b2e771325de209af0b22387
rubiii <me@rubiii.com>
2012-12-13 20:40:38 +0100

    no need to memoize the soap message

--- lib/savon/message.rb
+++ lib/savon/message.rb
@@ -16,13 +16,11 @@ module Savon
     def to_s
       return @message.to_s unless @message.kind_of? Hash
 
-      @string = begin
-        if @element_form_default == :qualified
-          @message = QualifiedMessage.new(@used_namespaces).to_hash(@message, [@operation_name])
-        end
-
-        Gyoku.xml @message, :element_form_default => @element_form_default, :namespace => @namespace_identifier
+      if @element_form_default == :qualified
+        @message = QualifiedMessage.new(@used_namespaces).to_hash(@message, [@operation_name])
       end
+
+      Gyoku.xml @message, :element_form_default => @element_form_default, :namespace => @namespace_identifier
     end
 
   end
