504846eafe55af50e62f271746e8d276ddab2e0a
rubiii <me@rubiii.com>
2012-12-21 18:34:22 +0100

    added simplecov

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 gemspec
 
 gem "httpclient", "~> 2.3.0"
+gem "simplecov", :require => false, :group => :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 require "bundler"
 Bundler.setup(:default, :development)
 
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
+
 require "savon"
 require "rspec"
 
