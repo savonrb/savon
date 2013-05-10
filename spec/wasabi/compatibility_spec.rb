f10b82c70cd5c15ffdb2e50a7c058ead2789e8c3
rubiii <me@rubiii.com>
2013-05-10 21:04:57 +0200

    added a simple compatibility spec for all fixtures
    this is based on Wasabi#inspect. the idea is to run as much of
    the code for as many fixtures as possible to find problems without
    having to write specs for every single feature.
    
    introduces four spec failures.

---
+++ spec/wasabi/compatibility_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Wasabi do
+
+  [
+
+    :authentication,
+    #:bookt,           # TODO: stub imports for bookt
+    #:bydexchange,     # TODO: stub imports for bydexchange
+    :economic,
+    :email_validation,
+    :geotrust,
+    :juniper,
+    :namespaced_actions,
+    :oracle,
+    :symbolic_endpoint,
+    :telefonkatalogen
+
+  ].each do |fixture_name|
+
+    it "works with #{fixture_name}.wsdl" do
+      wsdl = Wasabi.new fixture(fixture_name).read
+      wsdl.inspect.to_hash
+    end
+
+  end
+
+end
