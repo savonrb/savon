f5e050c27fcbc593d4d40c716dd8c5b13257db8a
rubiii <me@rubiii.com>
2013-05-12 15:21:46 +0200

    removed old code from the OperationBuilder

--- lib/wasabi/operation_builder.rb
+++ lib/wasabi/operation_builder.rb
@@ -15,11 +15,6 @@ class Wasabi
       service = @wsdl.documents.services.fetch(@service_name)
       port = service.ports.fetch(@port_name)
 
-      # for now we just find the first soap service and determine the soap version to use this way.
-      # we need to somehow let people determine the service (by name?) and port (by soap version?) to use.
-      #soap_service, soap_version = find_soap_service
-
-      #soap_port = soap_service.find_port_by_type(soap_version)
       endpoint = port.location
 
       binding = find_binding(port)
@@ -36,18 +31,6 @@ class Wasabi
     end
 
     private
-
-    #def find_soap_service
-      #@wsdl.documents.services.each do |name, service|
-        #soap_1_1_port = service.soap_1_1_port
-        #return [service, Wasabi::SOAP_1_1] if soap_1_1_port
-
-        #service.soap_1_2_port
-        #return [service, Wasabi::SOAP_1_2] if soap_1_2_port
-      #end
-
-      #raise 'Unable to find a SOAP service'
-    #end
 
     def find_binding(port)
       binding_name = port.binding.split(':').last
