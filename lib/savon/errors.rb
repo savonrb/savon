2cb4374ad2b1eb05a7d37144a58a6ecacf10e80c
rubiii <me@rubiii.com>
2013-05-31 13:08:06 +0200

    removed unused Error classes

--- lib/savon/errors.rb
+++ lib/savon/errors.rb
@@ -1,9 +1,5 @@
 class Savon
 
-  Error                 = Class.new(RuntimeError)
-  InitializationError   = Class.new(Error)
-  UnknownOptionError    = Class.new(Error)
-  UnknownOperationError = Class.new(Error)
-  InvalidResponseError  = Class.new(Error)
+  Error = Class.new(RuntimeError)
 
 end
