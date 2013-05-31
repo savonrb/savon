90f85f9aca8462173cc92d2932f37c066eca6361
rubiii <me@rubiii.com>
2013-05-31 17:07:07 +0200

    handle nil values and log missing elements

--- lib/savon/body.rb
+++ lib/savon/body.rb
@@ -4,6 +4,8 @@ class Savon
   class Body
 
     def initialize(envelope, parts)
+      @logger = Logging.logger[self]
+
       @envelope = envelope
       @parts = parts
     end
@@ -19,13 +21,17 @@ class Savon
 
     def build_elements(elements, message, xml)
       elements.each do |element|
-        tag = [element.name.to_sym]
+        name = element.name
+        symbol_name = name.to_sym
 
-        value = message.delete(element.name) ||
-                message.delete(element.name.to_sym)
+        value = extract_value(name, symbol_name, message)
 
-        # skip unspecified elements
-        next unless value
+        if value == :unspecified
+          @logger.debug("Skipping (optional?) element #{symbol_name.inspect} with no value.")
+          next
+        end
+
+        tag = [symbol_name]
 
         if element.form == 'qualified'
           nsid = @envelope.register_namespace(element.namespace)
@@ -51,6 +57,18 @@ class Savon
           end
 
         end
+      end
+    end
+
+    # Private: extracts the value from the message by name or symbol_name.
+    # Respects nil values and returns a special symbol for actual missing values.
+    def extract_value(name, symbol_name, message)
+      if message.include? name
+        message[name]
+      elsif message.include? symbol_name
+        message[symbol_name]
+      else
+        :unspecified
       end
     end
 
