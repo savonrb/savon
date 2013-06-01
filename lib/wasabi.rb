c8998f04f68ec2b84cb35a038c8ed3067df01c1e
rubiii <me@rubiii.com>
2013-06-01 17:42:41 +0200

    raise useful errors for invalid arguments

--- lib/wasabi.rb
+++ lib/wasabi.rb
@@ -28,10 +28,12 @@ class Wasabi
     @documents.service_name
   end
 
+  # XXX: check if we can get rid of this method.
   def target_namespace
     @documents.target_namespace
   end
 
+  # Public: Returns a Hash of services and ports defined by the WSDL.
   def services
     @services ||= begin
       services = {}
@@ -47,12 +49,49 @@ class Wasabi
     end
   end
 
+  # Public: Returns an Hash of operation names to Operations by service and port name.
   def operations(service_name, port_name)
+    verify_service_exists! service_name
+    verify_port_exists! service_name, port_name
+
     OperationBuilder.new(service_name, port_name, self).build
   end
 
+  # Public: Returns an Operation by service, port and operation name.
   def operation(service_name, port_name, operation_name)
-    operations(service_name, port_name)[operation_name]
+    operations = operations(service_name, port_name)
+    verify_operation_exists! operations, service_name, port_name, operation_name
+
+    operations[operation_name]
+  end
+
+  private
+
+  # Private: Raises a useful error in case the operation does not exist.
+  def verify_operation_exists!(operations, service_name, port_name, operation_name)
+    unless operations.include? operation_name
+      raise ArgumentError, "Unknown operation #{operation_name.inspect} for " \
+                           "service #{service_name.inspect} and port #{port_name.inspect}.\n" \
+                           "You may want to try one of #{operations.keys.inspect}."
+    end
+  end
+
+  # Private: Raises a useful error in case the service does not exist.
+  def verify_service_exists!(service_name)
+    unless services.include? service_name
+      raise ArgumentError, "Unknown service #{service_name.inspect}.\n" \
+                           "You may want to try one of #{services.keys.inspect}."
+    end
+  end
+
+  # Private: Raises a useful error in case the port does not exist.
+  def verify_port_exists!(service_name, port_name)
+    ports = services.fetch(service_name)[:ports]
+
+    unless ports.include? port_name
+      raise ArgumentError, "Unknown port #{port_name.inspect} for service #{service_name.inspect}.\n" \
+                           "You may want to try one of #{ports.keys.inspect}."
+    end
   end
 
 end
