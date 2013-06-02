0343f8d55f5d22d5058f565ecb131d4af020d654
rubiii <me@rubiii.com>
2013-06-02 17:41:29 +0200

    simplified Savon::Resolver

--- lib/savon/resolver.rb
+++ lib/savon/resolver.rb
@@ -10,24 +10,10 @@ class Savon
 
     def resolve(location)
       case location
-        when URL_PATTERN then load_from_remote(location)
+        when URL_PATTERN then @http.get(location)
         when XML_PATTERN then location
-        else                  load_from_disc(location)
+        else                  File.read(location)
       end
-    end
-
-    private
-
-    def load_from_remote(location)
-      if @http
-        @http.get(location)
-      else
-        raise Error, "HTTP adapter required to resolve #{location.inspect}"
-      end
-    end
-
-    def load_from_disc(location)
-      File.read(location)
     end
 
   end
