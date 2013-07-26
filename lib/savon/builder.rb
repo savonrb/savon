59e028039871bee8b63bd101945b7c1cb3d7ef1f
rubiii <me@rubiii.com>
2013-07-26 18:23:35 +0200

    renamed variable and fixed hash style for 1.8

--- lib/savon/builder.rb
+++ lib/savon/builder.rb
@@ -35,15 +35,16 @@ module Savon
     def to_s
       return @locals[:xml] if @locals.include? :xml
 
-      string = tag(builder, :Envelope, namespaces_with_globals) do |xml|
+      xml = tag(builder, :Envelope, namespaces_with_globals) do |xml|
         tag(xml, :Header) { xml << header.to_s } unless header.empty?
         tag(xml, :Body)   { xml.tag!(*namespaced_message_tag) { xml << message.to_s } }
       end
 
       if @globals[:encoding] && @globals[:encode_message]
-        string.encode!(@globals[:encoding], undef: :replace, replace:'')
+        xml.encode!(@globals[:encoding], :undef => :replace, :replace => '')
       end
-      string
+
+      xml
     end
 
     private
