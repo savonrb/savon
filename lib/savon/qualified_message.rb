fbe0d130f35996bccc422def7e382fa1468a5332
Jeremy McNevin <jmcnevin@users.noreply.github.com>
2017-08-02 13:48:09 -0500

    Multiple fixes for qualified message munger. (#676)
    * Fixes handling of content! and attributes!
    * General cleanup.

--- lib/savon/qualified_message.rb
+++ lib/savon/qualified_message.rb
@@ -2,49 +2,50 @@ require "gyoku"
 
 module Savon
   class QualifiedMessage
-
     def initialize(types, used_namespaces, key_converter)
-      @types = types
+      @types           = types
       @used_namespaces = used_namespaces
-      @key_converter = key_converter
+      @key_converter   = key_converter
     end
 
     def to_hash(hash, path)
-      return hash unless hash
-      return hash.map { |value| to_hash(value, path) } if hash.kind_of?(Array)
-      return hash.to_s unless hash.kind_of? Hash
+      return unless hash
+      return hash.map { |value| to_hash(value, path) } if hash.is_a?(Array)
+      return hash.to_s unless hash.is_a?(Hash)
 
-      hash.inject({}) do |newhash, (key, value)|
-        if key == :order!
-          add_namespaces_to_values(value, path)
-          newhash.merge(key => value)
+      hash.each_with_object({}) do |(key, value), newhash|
+        case key
+        when :order!
+          newhash[key] = add_namespaces_to_values(value, path)
+        when :attributes!, :content!
+          newhash[key] = to_hash(value, path)
         else
-          translated_key = Gyoku.xml_tag(key, :key_converter => @key_converter).to_s
-          translated_key << "!" if key[-1] == "!"
-          newpath = path + [translated_key]
-
-          if @used_namespaces[newpath]
-            newhash.merge(
-              "#{@used_namespaces[newpath]}:#{translated_key}" =>
-                to_hash(value, @types[newpath] ? [@types[newpath]] : newpath)
-            )
+          if key.to_s =~ /!$/
+            newhash[key] = value
           else
-            newhash.merge(translated_key => value)
+            translated_key  = translate_tag(key)
+            newkey          = add_namespaces_to_values(key, path).first
+            newpath         = path + [translated_key]
+            newhash[newkey] = to_hash(value, newpath)
           end
         end
+        newhash
       end
     end
 
     private
 
-    def add_namespaces_to_values(values, path)
-      values.collect! { |value|
-        camelcased_value = Gyoku.xml_tag(value, :key_converter => @key_converter)
-        namespace_path = path + [camelcased_value.to_s]
-        namespace = @used_namespaces[namespace_path]
-        "#{namespace.blank? ? '' : namespace + ":"}#{camelcased_value}"
-      }
+    def translate_tag(key)
+      Gyoku.xml_tag(key, :key_converter => @key_converter).to_s
     end
 
+    def add_namespaces_to_values(values, path)
+      Array(values).collect do |value|
+        translated_value = translate_tag(value)
+        namespace_path   = path + [translated_value]
+        namespace        = @used_namespaces[namespace_path]
+        namespace.blank? ? value : "#{namespace}:#{translated_value}"
+      end
+    end
   end
 end
