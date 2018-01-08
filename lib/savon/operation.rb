5385a1d1450135724d7e3f34644a52c96af34e19
007lva <007.lva@gmail.com>
2018-01-08 22:46:33 +0100

    Add magic comment to freeze string literals

--- lib/savon/operation.rb
+++ lib/savon/operation.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "savon/options"
 require "savon/block_interface"
 require "savon/request"
--- spec/support/fixture.rb
+++ spec/support/fixture.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Fixture
 
   TYPES = { :gzip => "gz", :response => "xml", :wsdl => "xml" }
