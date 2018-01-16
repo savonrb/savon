02c5b82a155dbebb45ec8438e1200dddebcb22d1
Avreimyki <35486809+Avreimyki@users.noreply.github.com>
2018-01-16 12:24:03 +0200

    fix namespace issue
    I had a small mistake in my previous fix

--- lib/savon/qualified_message.rb
+++ lib/savon/qualified_message.rb
@@ -15,19 +15,19 @@ module Savon
 
       hash.each_with_object({}) do |(key, value), newhash|
         case key
-        when :order!
-          newhash[key] = add_namespaces_to_values(value, path)
-        when :attributes!, :content!
-          newhash[key] = to_hash(value, path)
-        else
-          if key.to_s =~ /!$/
-            newhash[key] = value
+          when :order!
+            newhash[key] = add_namespaces_to_values(value, path)
+          when :attributes!, :content!
+            newhash[key] = to_hash(value, path)
           else
-            translated_key  = translate_tag(key)
-            newkey          = add_namespaces_to_values(key, path).first
-            newpath         = path + [translated_key]
-            newhash[newkey] = to_hash(value, newpath)
-          end
+            if key.to_s =~ /!$/
+              newhash[key] = value
+            else
+              translated_key  = translate_tag(key)
+              newkey          = add_namespaces_to_values(key, path).first
+              newpath         = path + [translated_key]
+              newhash[newkey] = to_hash(value, newpath)
+            end
         end
         newhash
       end
@@ -43,9 +43,30 @@ module Savon
       Array(values).collect do |value|
         translated_value = translate_tag(value)
         namespace_path   = path + [translated_value]
-        namespace        = @used_namespaces[namespace_path]
+        namespace        = namespace_deep_look(namespace_path)
         namespace.blank? ? value : "#{namespace}:#{translated_value}"
       end
     end
+
+    def namespace_deep_look(path)
+      down_path = path.dup
+      while !down_path.empty?
+        namespace = namespace_by_path(down_path)
+        return namespace if !namespace.nil?
+        down_path.shift
+      end
+      up_path = path.dup
+      while !up_path.empty?
+        up_path.pop
+        namespace = namespace_deep_look(up_path) if !up_path.empty?
+        return namespace if !namespace.nil?
+      end
+    end
+
+    def namespace_by_path(namespace_path)
+      namespace_path_type = @types.find{|key,val| key.map{|e| e.to_s.underscore}  == namespace_path.map{|e| e.to_s.underscore}}
+      namespace_path_type.nil? ? nil : @used_namespaces[namespace_path_type[0]]
+    end
   end
 end
+
