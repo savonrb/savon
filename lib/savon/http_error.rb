6ff20ff9539f8927a50e2fb06da42f7d7fbe859f
007lva <007.lva@gmail.com>
2018-01-19 19:42:07 +0100

    Fix regresion on Savon::HTTPError#to_s

--- lib/savon/http_error.rb
+++ lib/savon/http_error.rb
@@ -15,7 +15,9 @@ module Savon
     attr_reader :http
 
     def to_s
-      "HTTP error (#{@http.code}): #{@http.body}" unless @http.body.empty?
+      String.new("HTTP error (#{@http.code})").tap do |str_error|
+        str_error << ": #{@http.body}" unless @http.body.empty?
+      end
     end
 
     def to_hash
