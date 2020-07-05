b2b545ac66ff0e944bc1d37e55046788ba3dea56
Whit Armstrong <armstrong.whit@gmail.com>
2020-07-05 16:53:13 -0400

    Fix: Remove xmlns:wsa's already added elsewhere; select Content-Type HTTP header based on SOAP version (#868)
    * remove unneeded xmlns:wsa, already added elsewhere
    * clean up Content-Type header and add MIME-Version
    * select header Content-Type based on SOAP version
    * xmlns:wsa attribute is not needed on wsa:MessageID. it's already included in the header
    
    Co-authored-by: Olle Jonsson <olle.jonsson@gmail.com>

--- lib/savon/header.rb
+++ lib/savon/header.rb
@@ -62,12 +62,7 @@ module Savon
        convert_to_xml({
          'wsa:Action' => @locals[:soap_action],
          'wsa:To' => @globals[:endpoint],
-         'wsa:MessageID' => "urn:uuid:#{SecureRandom.uuid}",
-         attributes!: {
-          'wsa:MessageID' => {
-            "xmlns:wsa" => "http://schemas.xmlsoap.org/ws/2004/08/addressing"
-          }
-         }
+         'wsa:MessageID' => "urn:uuid:#{SecureRandom.uuid}"
        })
     end
 
