f6294031cc1dfdfb425947dfface1fa1169305f6
Olle Jonsson <olle.jonsson@gmail.com>
2021-09-09 23:03:08 +0200

    test: Sort spec support files before requiring them (#965)
    To have a uniform order of require.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require "rspec"
 require "httpclient"
 
 support_files = File.expand_path("spec/support/**/*.rb")
-Dir[support_files].each { |file| require file }
+Dir[support_files].sort.each { |file| require file }
 
 RSpec.configure do |config|
   config.include SpecSupport
