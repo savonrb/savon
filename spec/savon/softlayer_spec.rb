c01e7593c5683e6e3a21cbe389043246c0ecb032
Victor Silvano <victorpsilvano@gmail.com>
2022-09-02 17:00:18 +0100

    test: Adding test for the envelope namespaces

--- spec/savon/softlayer_spec.rb
+++ spec/savon/softlayer_spec.rb
@@ -22,7 +22,17 @@ RSpec.describe Savon::Builder do
 
       locals = Savon::LocalOptions.new(message)
       builder = Savon::Builder.new(:create_object, wsdl, globals, locals)
-      expect(builder.to_s).to include('<env:Envelope')
+
+      envelope = Nokogiri::XML(builder.to_s).xpath('./env:Envelope').first
+
+      expect(envelope.namespaces['xmlns:xsd']).to eq("http://www.w3.org/2001/XMLSchema")
+      expect(envelope.namespaces['xmlns:xsi']).to eq("http://www.w3.org/2001/XMLSchema-instance")
+      expect(envelope.namespaces['xmlns:tns']).to eq("http://api.service.softlayer.com/soap/v3/")
+      expect(envelope.namespaces['xmlns:env']).to eq("http://schemas.xmlsoap.org/soap/envelope/")
+      expect(envelope.namespaces['xmlns']).to eq("http://schemas.xmlsoap.org/wsdl/")
+      expect(envelope.namespaces['xmlns:soap']).to eq("http://schemas.xmlsoap.org/wsdl/soap/")
+      expect(envelope.namespaces['xmlns:soap-enc']).to eq("http://schemas.xmlsoap.org/soap/encoding/")
+      expect(envelope.namespaces['xmlns:wsdl']).to eq("http://schemas.xmlsoap.org/wsdl/")
     end
   end
 end
