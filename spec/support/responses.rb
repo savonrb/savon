4cebdf866981c3e838ab2133c008d3756e5b2c26
lridge <lridge@igsenergy.com>
2024-01-08 12:41:20 -0500

    use from initializer for env for 2.7 compat

--- spec/support/responses.rb
+++ spec/support/responses.rb
@@ -1,7 +1,7 @@
 class Responses
   class << self
     def mock_faraday(code, headers, body)
-      env = Faraday::Env.new(status: code, response_headers: headers, response_body: body)
+      env = Faraday::Env.from(status: code, response_headers: headers, response_body: body)
       Faraday::Response.new(env)
     end
   end
