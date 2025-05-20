c0c30778363fc8956af8f4bf63b24913fe225c14
Jacob Kjeldahl <jacob@kjeldahl.it>
2025-05-20 10:30:19 +0200

    Increases the number of builds to enable https://github.com/savonrb/akami/pull/25
    I have no idea why the document needs to be built 2 and now 3 times
    and it seems to always have been a mystery https://github.com/savonrb/savon/commit/89a15f12cdb48163e313e53664a54279f07bf20d

--- lib/savon/builder.rb
+++ lib/savon/builder.rb
@@ -63,7 +63,7 @@ module Savon
       if @signature
         @signature.document = xml_result
 
-        2.times do
+        3.times do
           @header = nil
           @signature.document = build_xml
         end
