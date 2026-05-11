savonrb/savon

commit 725855d6f384bae65bd5e3ce36cc5d1456e9212d

Faraday (#998)
* replace httpi with faraday, pull in rubyntlm since it'll be needed for ntlm auth handshakes

lib/savon/request.rb
@@ -1,61 +1,87 @@
 # frozen_string_literal: true
-require "httpi"
+require "faraday"
 
 module Savon
   class HTTPRequest
 
-    def initialize(globals, http_request = nil)
+    def initialize(globals, connection = nil)
       @globals = globals
-      @http_request = http_request || HTTPI::Request.new
-    end
-
-    def build
-      @http_request
+      @connection = connection || Faraday::Connection.new
     end
 
     private
 
     def configure_proxy
-      @http_request.proxy = @globals[:proxy] if @globals.include? :proxy
+      connection.proxy = @globals[:proxy] if @globals.include? :proxy
     end
 
     def configure_timeouts
-      @http_request.open_timeout = @globals[:open_timeout] if @globals.include? :open_timeout
-      @http_request.read_timeout = @globals[:read_timeout] if @globals.include? :read_timeout
-      @http_request.write_timeout = @globals[:write_timeout] if @globals.include? :write_timeout
+      connection.options.open_timeout = @globals[:open_timeout] if @globals.include? :open_timeout
+      connection.options.read_timeout = @globals[:read_timeout] if @globals.include? :read_timeout
+      connection.options.write_timeout = @globals[:write_timeout] if @globals.include? :write_timeout
     end
 
     def configure_ssl
-      @http_request.auth.ssl.ssl_version   = @globals[:ssl_version]       if @globals.include? :ssl_version
-      @http_request.auth.ssl.min_version   = @globals[:ssl_min_version]   if @globals.include? :ssl_min_version
-      @http_request.auth.ssl.max_version   = @globals[:ssl_max_version]   if @globals.include? :ssl_max_version
+      connection.ssl.verify          = @globals[:ssl_verify]        if @globals.include? :ssl_verify
+      connection.ssl.ca_file         = @globals[:ssl_ca_cert_file]  if @globals.include? :ssl_ca_cert_file
+      connection.ssl.verify_hostname = @globals[:verify_hostname]   if @globals.include? :verify_hostname
+      connection.ssl.ca_path         = @globals[:ssl_ca_cert_path]  if @globals.include? :ssl_ca_cert_path
+      connection.ssl.verify_mode     = @globals[:ssl_verify_mode]   if @globals.include? :ssl_verify_mode
+      connection.ssl.cert_store      = @globals[:ssl_cert_store]    if @globals.include? :ssl_cert_store
+      connection.ssl.client_cert     = @globals[:ssl_cert]          if @globals.include? :ssl_cert
+      connection.ssl.client_key      = @globals[:ssl_cert_key]      if @globals.include? :ssl_cert_key
+      connection.ssl.certificate     = @globals[:ssl_certificate]   if @globals.include? :ssl_certificate
+      connection.ssl.private_key     = @globals[:ssl_private_key]   if @globals.include? :ssl_private_key
+      connection.ssl.verify_depth    = @globals[:verify_depth]      if @globals.include? :verify_depth
+      connection.ssl.version         = @globals[:ssl_version]       if @globals.include? :ssl_version
+      connection.ssl.min_version     = @globals[:ssl_min_version]   if @globals.include? :ssl_min_version
+      connection.ssl.max_version     = @globals[:ssl_max_version]   if @globals.include? :ssl_max_version
 
-      @http_request.auth.ssl.verify_mode   = @globals[:ssl_verify_mode]   if @globals.include? :ssl_verify_mode
-      @http_request.auth.ssl.ciphers       = @globals[:ssl_ciphers]       if @globals.include? :ssl_ciphers
+      # No Faraday Equivalent out of box, see: https://lostisland.github.io/faraday/#/customization/ssl-options
+      # connection.ssl.cert_file       = @globals[:ssl_cert_file]     if @globals.include? :ssl_cert_file
+      # connection.ssl.cert_key_file   = @globals[:ssl_cert_key_file] if @globals.include? :ssl_cert_key_file
+      # connection.ssl.ca_cert         = @globals[:ssl_ca_cert]       if @globals.include? :ssl_ca_cert
+      # connection.ssl.ciphers         = @globals[:ssl_ciphers]       if @globals.include? :ssl_ciphers
+      # connection.ssl.cert_key_password = @globals[:ssl_cert_key_password] if @globals.include? :ssl_cert_key_password
 
-      @http_request.auth.ssl.cert_key_file = @globals[:ssl_cert_key_file] if @globals.include? :ssl_cert_key_file
-      @http_request.auth.ssl.cert_key      = @globals[:ssl_cert_key]      if @globals.include? :ssl_cert_key
-      @http_request.auth.ssl.cert_file     = @globals[:ssl_cert_file]     if @globals.include? :ssl_cert_file
-      @http_request.auth.ssl.cert          = @globals[:ssl_cert]          if @globals.include? :ssl_cert
-      @http_request.auth.ssl.ca_cert_file  = @globals[:ssl_ca_cert_file]  if @globals.include? :ssl_ca_cert_file
-      @http_request.auth.ssl.ca_cert_path  = @globals[:ssl_ca_cert_path]  if @globals.include? :ssl_ca_cert_path
-      @http_request.auth.ssl.ca_cert       = @globals[:ssl_ca_cert]       if @globals.include? :ssl_ca_cert
-      @http_request.auth.ssl.cert_store    = @globals[:ssl_cert_store]    if @globals.include? :ssl_cert_store
-
-      @http_request.auth.ssl.cert_key_password = @globals[:ssl_cert_key_password] if @globals.include? :ssl_cert_key_password
     end
 
     def configure_auth
-      @http_request.auth.basic(*@globals[:basic_auth])   if @globals.include? :basic_auth
-      @http_request.auth.digest(*@globals[:digest_auth]) if @globals.include? :digest_auth
-      @http_request.auth.ntlm(*@globals[:ntlm])          if @globals.include? :ntlm
+      basic_auth if @globals.include?(:basic_auth)
+      ntlm_auth if @globals.include?(:ntlm)
+    end
+
+    def basic_auth
+      connection.request(:authorization, :basic, *@globals[:basic_auth])
+    end
+
+    def ntlm_auth
+      begin
+        require 'rubyntlm'
+        require 'faraday/net_http_persistent'
+        connection.adapter :net_http_persistent, pool_size: 5
+      rescue LoadError
+        raise LoadError, 'Using NTLM Auth requires both `rubyntlm` and `faraday-net_http_persistent` to be installed.'
+      end
     end
 
     def configure_redirect_handling
-      if @globals.include? :follow_redirects
-        @http_request.follow_redirect = @globals[:follow_redirects]
+      if @globals[:follow_redirects]
+        require 'faraday/follow_redirects'
+        connection.response :follow_redirects
       end
     end
+
+    def configure_adapter
+      connection.adapter(*@globals[:adapter]) unless @globals[:adapter].nil?
+    end
+
+    def configure_logging
+      connection.response(:logger, nil, headers: @globals[:log_headers], level: @globals[:logger].level) if @globals[:log]
+    end
+
+    protected
+    attr_reader :connection
   end
 
   class WSDLRequest < HTTPRequest
@@ -63,18 +89,18 @@ module Savon
     def build
       configure_proxy
       configure_timeouts
-      configure_headers
       configure_ssl
       configure_auth
-      configure_redirect_handling
-
-      @http_request
+      configure_adapter
+      configure_logging
+      configure_headers
+      connection
     end
 
     private
 
     def configure_headers
-      @http_request.headers = @globals[:headers] if @globals.include? :headers
+      connection.headers = @globals[:headers] if @globals.include? :headers
     end
   end
 
@@ -88,26 +114,34 @@ module Savon
     def build(options = {})
       configure_proxy
       configure_timeouts
-      configure_headers options[:soap_action], options[:headers]
-      configure_cookies options[:cookies]
       configure_ssl
       configure_auth
+      configure_headers(options[:soap_action], options[:headers])
+      configure_cookies(options[:cookies])
+      configure_adapter
+      configure_logging
       configure_redirect_handling
-
-      @http_request
+      yield(connection) if block_given?
+      connection
     end
 
     private
 
     def configure_cookies(cookies)
-      @http_request.set_cookies(cookies) if cookies
+      connection.headers['Cookie'] = cookies.map do |key, value|
+        if value.nil?
+          key
+        else
+          "#{key}=#{value}"
+        end
+      end.join('; ') if cookies
     end
 
     def configure_headers(soap_action, headers)
-      @http_request.headers = @globals[:headers] if @globals.include? :headers
-      @http_request.headers.merge!(headers) if headers
-      @http_request.headers["SOAPAction"]   ||= %{"#{soap_action}"} if soap_action
-      @http_request.headers["Content-Type"] ||= CONTENT_TYPE[@globals[:soap_version]] % @globals[:encoding]
+      connection.headers = @globals[:headers] if @globals.include? :headers
+      connection.headers.merge!(headers) if headers
+      connection.headers["SOAPAction"]   ||= %{"#{soap_action}"} if soap_action
+      connection.headers["Content-Type"] ||= CONTENT_TYPE[@globals[:soap_version]] % @globals[:encoding]
     end
   end
 end

spec/savon/request_spec.rb
@@ -5,17 +5,18 @@ require "integration/support/server"
 RSpec.describe Savon::WSDLRequest do
 
   let(:globals)      { Savon::GlobalOptions.new }
-  let(:http_request) { HTTPI::Request.new }
+  let(:http_connection) { Faraday::Connection.new }
   let(:ciphers)      { OpenSSL::Cipher.ciphers }
 
   def new_wsdl_request
-    Savon::WSDLRequest.new(globals, http_request)
+    Savon::WSDLRequest.new(globals, http_connection)
   end
 
-  describe "#build" do
-    it "returns an HTTPI::Request" do
+  describe "build" do
+    it "returns an Faraday::Request" do
       wsdl_request = Savon::WSDLRequest.new(globals)
-      expect(wsdl_request.build).to be_an(HTTPI::Request)
+      result = wsdl_request.build
+      expect(result).to be_an(Faraday::Connection)
     end
 
     describe "headers" do
@@ -35,13 +36,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "proxy" do
       it "is set when specified" do
         globals.proxy("http://proxy.example.com")
-        http_request.expects(:proxy=).with("http://proxy.example.com")
+        http_connection.expects(:proxy=).with("http://proxy.example.com")
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.expects(:proxy=).never
+        http_connection.expects(:proxy=).never
         new_wsdl_request.build
       end
     end
@@ -49,13 +50,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "open timeout" do
       it "is set when specified" do
         globals.open_timeout(22)
-        http_request.expects(:open_timeout=).with(22)
+        http_connection.options.expects(:open_timeout=).with(22)
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.expects(:open_timeout=).never
+        http_connection.options.expects(:open_timeout=).never
         new_wsdl_request.build
       end
     end
@@ -63,13 +64,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "read timeout" do
       it "is set when specified" do
         globals.read_timeout(33)
-        http_request.expects(:read_timeout=).with(33)
+        http_connection.options.expects(:read_timeout=).with(33)
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.expects(:read_timeout=).never
+        http_connection.options.expects(:read_timeout=).never
         new_wsdl_request.build
       end
     end
@@ -77,13 +78,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "write timeout" do
       it "is set when specified" do
         globals.write_timeout(44)
-        http_request.expects(:write_timeout=).with(44)
+        http_connection.options.expects(:write_timeout=).with(44)
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.expects(:read_timeout=).never
+        http_connection.expects(:read_timeout=).never
         new_wsdl_request.build
       end
     end
@@ -91,13 +92,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "ssl version" do
       it "is set when specified" do
         globals.ssl_version(:TLSv1)
-        http_request.auth.ssl.expects(:ssl_version=).with(:TLSv1)
+        http_connection.ssl.expects(:version=).with(:TLSv1)
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.ssl.expects(:ssl_version=).never
+        http_connection.ssl.expects(:version=).never
         new_wsdl_request.build
       end
     end
@@ -105,13 +106,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "ssl min_version" do
       it "is set when specified" do
         globals.ssl_min_version(:TLS1_2)
-        http_request.auth.ssl.expects(:min_version=).with(:TLS1_2)
+        http_connection.ssl.expects(:min_version=).with(:TLS1_2)
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.ssl.expects(:min_version=).never
+        http_connection.ssl.expects(:min_version=).never
         new_wsdl_request.build
       end
     end
@@ -119,13 +120,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "ssl max_version" do
       it "is set when specified" do
         globals.ssl_max_version(:TLS1_2)
-        http_request.auth.ssl.expects(:max_version=).with(:TLS1_2)
+        http_connection.ssl.expects(:max_version=).with(:TLS1_2)
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.ssl.expects(:max_version=).never
+        http_connection.ssl.expects(:max_version=).never
         new_wsdl_request.build
       end
     end
@@ -133,122 +134,13 @@ RSpec.describe Savon::WSDLRequest do
     describe "ssl verify mode" do
       it "is set when specified" do
         globals.ssl_verify_mode(:peer)
-        http_request.auth.ssl.expects(:verify_mode=).with(:peer)
+        http_connection.ssl.expects(:verify_mode=).with(:peer)
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.ssl.expects(:verify_mode=).never
-        new_wsdl_request.build
-      end
-    end
-
-    describe "ssl ciphers" do
-      it "is set when specified" do
-        globals.ssl_ciphers(ciphers)
-        http_request.auth.ssl.expects(:ciphers=).with(ciphers)
-
-        new_wsdl_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:ciphers=).never
-        new_wsdl_request.build
-      end
-    end
-
-    describe "ssl cert key file" do
-      it "is set when specified" do
-        cert_key = File.expand_path("../../fixtures/ssl/client_key.pem", __FILE__)
-        globals.ssl_cert_key_file(cert_key)
-        http_request.auth.ssl.expects(:cert_key_file=).with(cert_key)
-
-        new_wsdl_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:cert_key_file=).never
-        new_wsdl_request.build
-      end
-    end
-
-    describe "ssl cert key password" do
-      it "is set when specified" do
-        the_pass = "secure-password!42"
-        globals.ssl_cert_key_password(the_pass)
-        http_request.auth.ssl.expects(:cert_key_password=).with(the_pass)
-
-        new_wsdl_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:cert_key_password=).never
-        new_wsdl_request.build
-      end
-    end
-
-    describe "ssl encrypted cert key file" do
-      describe "set with an invalid decrypting password" do
-        it "fails when attempting to use the SSL private key" do
-          skip("JRuby: find out why this does not raise an error!") if RUBY_PLATFORM == 'java'
-          pass = "wrong-password"
-          key  = File.expand_path("../../fixtures/ssl/client_encrypted_key.pem", __FILE__)
-          cert = File.expand_path("../../fixtures/ssl/client_encrypted_key_cert.pem", __FILE__)
-
-          globals.ssl_cert_file(cert)
-          globals.ssl_cert_key_password(pass)
-          globals.ssl_cert_key_file(key)
-
-          new_wsdl_request.build
-
-          expect { http_request.auth.ssl.cert_key }.to raise_error OpenSSL::PKey::PKeyError
-        end
-      end
-
-      describe "set with a valid decrypting password" do
-        it "handles SSL private keys properly" do
-          pass = "secure-password!42"
-          key  = File.expand_path("../../fixtures/ssl/client_encrypted_key.pem", __FILE__)
-          cert = File.expand_path("../../fixtures/ssl/client_encrypted_key_cert.pem", __FILE__)
-
-          globals.ssl_cert_file(cert)
-          globals.ssl_cert_key_password(pass)
-          globals.ssl_cert_key_file(key)
-
-          new_wsdl_request.build
-
-          expect(http_request.auth.ssl.cert_key.to_s).to match(/BEGIN RSA PRIVATE KEY/)
-        end
-      end
-    end
-
-    describe "ssl cert file" do
-      it "is set when specified" do
-        cert = File.expand_path("../../fixtures/ssl/client_cert.pem", __FILE__)
-        globals.ssl_cert_file(cert)
-        http_request.auth.ssl.expects(:cert_file=).with(cert)
-
-        new_wsdl_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:cert_file=).never
-        new_wsdl_request.build
-      end
-    end
-
-    describe "ssl ca cert file" do
-      it "is set when specified" do
-        ca_cert = File.expand_path("../../fixtures/ssl/client_cert.pem", __FILE__)
-        globals.ssl_ca_cert_file(ca_cert)
-        http_request.auth.ssl.expects(:ca_cert_file=).with(ca_cert)
-
-        new_wsdl_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:ca_cert_file=).never
+        http_connection.ssl.expects(:verify_mode=).never
         new_wsdl_request.build
       end
     end
@@ -256,41 +148,33 @@ RSpec.describe Savon::WSDLRequest do
     describe "basic auth" do
       it "is set when specified" do
         globals.basic_auth("luke", "secret")
-        http_request.auth.expects(:basic).with("luke", "secret")
+        http_connection.expects(:request).with(:authorization, :basic,"luke", "secret")
 
         new_wsdl_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.expects(:basic).never
-        new_wsdl_request.build
-      end
-    end
-
-    describe "digest auth" do
-      it "is set when specified" do
-        globals.digest_auth("lea", "top-secret")
-        http_request.auth.expects(:digest).with("lea", "top-secret")
-
-        new_wsdl_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.expects(:digest).never
+        http_connection.expects(:request).with{|args| args.include?(:basic)}.never
         new_wsdl_request.build
       end
     end
 
     describe "ntlm auth" do
-      it "is set when specified" do
+      it 'tries to load ntlm when set' do
         globals.ntlm("han", "super-secret")
-        http_request.auth.expects(:ntlm).with("han", "super-secret")
+        new_wsdl_request.build
+        expect(require 'rubyntlm').to be(false)
+      end
+
+      it "applies net-http-persistent when set" do
+        globals.ntlm("han", "super-secret")
+        http_connection.expects(:adapter).with{|params| params == :net_http_persistent}.at_least_once
 
         new_wsdl_request.build
       end
 
-      it "is not set otherwise" do
-        http_request.auth.expects(:ntlm).never
+      it "does not apply net-http-persistent when not set" do
+        http_connection.expects(:adapter).with(:net_http_persistent, pool_size: 5).never
         new_wsdl_request.build
       end
     end
@@ -301,43 +185,49 @@ end
 RSpec.describe Savon::SOAPRequest do
 
   let(:globals)      { Savon::GlobalOptions.new }
-  let(:http_request) { HTTPI::Request.new }
+  let(:http_connection) { Faraday::Connection.new }
   let(:ciphers)      { OpenSSL::Cipher.ciphers }
 
   def new_soap_request
-    Savon::SOAPRequest.new(globals, http_request)
+    Savon::SOAPRequest.new(globals, http_connection)
   end
 
-  describe "#build" do
-    it "returns an HTTPI::Request" do
+  describe "build" do
+    it "returns an Faraday::Request" do
       soap_request = Savon::SOAPRequest.new(globals)
-      expect(soap_request.build).to be_an(HTTPI::Request)
+      expect(soap_request.build).to be_an(Faraday::Connection)
     end
 
     describe "proxy" do
       it "is set when specified" do
         globals.proxy("http://proxy.example.com")
-        http_request.expects(:proxy=).with("http://proxy.example.com")
+        http_connection.expects(:proxy=).with("http://proxy.example.com")
 
         new_soap_request.build
       end
 
       it "is not set otherwise" do
-        http_request.expects(:proxy=).never
+        http_connection.expects(:proxy=).never
         new_soap_request.build
       end
     end
 
     describe "cookies" do
       it "sets the given cookies" do
-        cookies = [HTTPI::Cookie.new("some-cookie=choc-chip; Path=/; HttpOnly")]
+        cookies = {
+          'some-cookie': 'choc-chip',
+          path: '/',
+          HttpOnly: nil
+        }
 
-        http_request.expects(:set_cookies).with(cookies)
+        http_connection.headers.expects(:[]=).at_least_once
+        http_connection.headers.expects(:[]=).with('Cookie', 'some-cookie=choc-chip; path=/; HttpOnly').at_least_once
         new_soap_request.build(:cookies => cookies)
       end
 
       it "does not set the cookies if there are none" do
-        http_request.expects(:set_cookies).never
+        http_connection.headers.expects(:[]=).at_least_once
+        http_connection.expects(:[]=).with('Cookie').never
         new_soap_request.build
       end
     end
@@ -345,13 +235,13 @@ RSpec.describe Savon::SOAPRequest do
     describe "open timeout" do
       it "is set when specified" do
         globals.open_timeout(22)
-        http_request.expects(:open_timeout=).with(22)
+        http_connection.options.expects(:open_timeout=).with(22)
 
         new_soap_request.build
       end
 
       it "is not set otherwise" do
-        http_request.expects(:open_timeout=).never
+        http_connection.options.expects(:open_timeout=).never
         new_soap_request.build
       end
     end
@@ -359,13 +249,13 @@ RSpec.describe Savon::SOAPRequest do
     describe "read timeout" do
       it "is set when specified" do
         globals.read_timeout(33)
-        http_request.expects(:read_timeout=).with(33)
+        http_connection.options.expects(:read_timeout=).with(33)
 
         new_soap_request.build
       end
 
       it "is not set otherwise" do
-        http_request.expects(:read_timeout=).never
+        http_connection.options.expects(:read_timeout=).never
         new_soap_request.build
       end
     end
@@ -436,13 +326,13 @@ RSpec.describe Savon::SOAPRequest do
     describe "ssl version" do
       it "is set when specified" do
         globals.ssl_version(:TLSv1)
-        http_request.auth.ssl.expects(:ssl_version=).with(:TLSv1)
+        http_connection.ssl.expects(:version=).with(:TLSv1)
 
         new_soap_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.ssl.expects(:ssl_version=).never
+        http_connection.ssl.expects(:version=).never
         new_soap_request.build
       end
     end
@@ -450,87 +340,13 @@ RSpec.describe Savon::SOAPRequest do
     describe "ssl verify mode" do
       it "is set when specified" do
         globals.ssl_verify_mode(:peer)
-        http_request.auth.ssl.expects(:verify_mode=).with(:peer)
+        http_connection.ssl.expects(:verify_mode=).with(:peer)
 
         new_soap_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.ssl.expects(:verify_mode=).never
-        new_soap_request.build
-      end
-    end
-
-    describe "ssl ciphers" do
-      it "is set when specified" do
-        globals.ssl_ciphers(ciphers)
-        http_request.auth.ssl.expects(:ciphers=).with(ciphers)
-
-        new_soap_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:ciphers=).never
-        new_soap_request.build
-      end
-    end
-
-    describe "ssl cert key file" do
-      it "is set when specified" do
-        cert_key = File.expand_path("../../fixtures/ssl/client_key.pem", __FILE__)
-        globals.ssl_cert_key_file(cert_key)
-        http_request.auth.ssl.expects(:cert_key_file=).with(cert_key)
-
-        new_soap_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:cert_key_file=).never
-        new_soap_request.build
-      end
-    end
-
-    describe "ssl cert key password" do
-      it "is set when specified" do
-        the_pass = "secure-password!42"
-        globals.ssl_cert_key_password(the_pass)
-        http_request.auth.ssl.expects(:cert_key_password=).with(the_pass)
-
-        new_soap_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:cert_key_password=).never
-        new_soap_request.build
-      end
-    end
-
-    describe "ssl cert file" do
-      it "is set when specified" do
-        cert = File.expand_path("../../fixtures/ssl/client_cert.pem", __FILE__)
-        globals.ssl_cert_file(cert)
-        http_request.auth.ssl.expects(:cert_file=).with(cert)
-
-        new_soap_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:cert_file=).never
-        new_soap_request.build
-      end
-    end
-
-    describe "ssl ca cert file" do
-      it "is set when specified" do
-        ca_cert = File.expand_path("../../fixtures/ssl/client_cert.pem", __FILE__)
-        globals.ssl_ca_cert_file(ca_cert)
-        http_request.auth.ssl.expects(:ca_cert_file=).with(ca_cert)
-
-        new_soap_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.ssl.expects(:ca_cert_file=).never
+        http_connection.ssl.expects(:verify_mode=).never
         new_soap_request.build
       end
     end
@@ -538,41 +354,25 @@ RSpec.describe Savon::SOAPRequest do
     describe "basic auth" do
       it "is set when specified" do
         globals.basic_auth("luke", "secret")
-        http_request.auth.expects(:basic).with("luke", "secret")
-
+        http_connection.expects(:request).with(:authorization, :basic, "luke", "secret")
         new_soap_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.expects(:basic).never
-        new_soap_request.build
-      end
-    end
-
-    describe "digest auth" do
-      it "is set when specified" do
-        globals.digest_auth("lea", "top-secret")
-        http_request.auth.expects(:digest).with("lea", "top-secret")
-
-        new_soap_request.build
-      end
-
-      it "is not set otherwise" do
-        http_request.auth.expects(:digest).never
+        http_connection.expects(:request).with(:authorization, :basic, "luke", 'secret').never
         new_soap_request.build
       end
     end
 
     describe "ntlm auth" do
-      it "is set when specified" do
+      it "uses the net-http-persistent adapter in faraday" do
         globals.ntlm("han", "super-secret")
-        http_request.auth.expects(:ntlm).with("han", "super-secret")
-
+        http_connection.expects(:adapter).with(:net_http_persistent, {:pool_size => 5})
         new_soap_request.build
       end
 
       it "is not set otherwise" do
-        http_request.auth.expects(:ntlm).never
+        http_connection.expects(:adapter).with(:net_http_persistent, {:pool_size => 5}).never
         new_soap_request.build
       end
     end

lib/savon.rb
@@ -7,6 +7,14 @@ module Savon
   UnknownOperationError = Class.new(Error)
   InvalidResponseError  = Class.new(Error)
 
+  class DeprecatedOptionError < Error
+    attr_accessor :option
+    def initialize(option)
+      @option = option
+      super("#{option} is deprecated as it is not supported in Faraday")
+    end
+  end
+
   def self.client(globals = {}, &block)
     Client.new(globals, &block)
   end

lib/savon/http_error.rb
@@ -4,7 +4,7 @@ module Savon
   class HTTPError < Error
 
     def self.present?(http)
-      http.error?
+      !http.success?
     end
 
     def initialize(http)
@@ -14,13 +14,13 @@ module Savon
     attr_reader :http
 
     def to_s
-      String.new("HTTP error (#{@http.code})").tap do |str_error|
+      String.new("HTTP error (#{@http.status})").tap do |str_error|
         str_error << ": #{@http.body}" unless @http.body.empty?
       end
     end
 
     def to_hash
-      { :code => @http.code, :headers => @http.headers, :body => @http.body }
+      { :code => @http.status, :headers => @http.headers, :body => @http.body }
     end
 
   end

commit 2e64cbb6c62610908471b808f80e1cfafa22ca32

Fix `WSDLRequest` ignoring `follow_redirects` option
725855d accidentally dropped the `configure_redirect_handling`
call from `WSDLRequest#build` while keeping it in `SOAPRequest#build`.

lib/savon/request.rb
@@ -85,7 +85,7 @@ module Savon
         connection.use(*middleware_args)
       end
     end
-    
+
     def configure_gzip
       if connection.headers['Accept-Encoding'] && connection.headers['Accept-Encoding'].include?('gzip')
         connection.request :gzip
@@ -106,6 +106,7 @@ module Savon
       configure_adapter
       configure_middlewares
       configure_logging
+      configure_redirect_handling
       configure_headers
       configure_gzip
       connection

spec/savon/request_spec.rb
@@ -196,6 +196,20 @@ RSpec.describe Savon::WSDLRequest do
         new_wsdl_request.build
       end
     end
+
+    describe "follow redirects" do
+      it "is set when specified" do
+        globals.follow_redirects(true)
+        http_connection.expects(:response).with(:follow_redirects)
+
+        new_wsdl_request.build
+      end
+
+      it "is not set otherwise" do
+        http_connection.expects(:response).with(:follow_redirects).never
+        new_wsdl_request.build
+      end
+    end
   end
 end
 